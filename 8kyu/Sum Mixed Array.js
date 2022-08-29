60c37a5ebf144caf1a59f40fcdb04769b8395968
devmev10 <ravitokas90@gmail.com>
2022-08-29 16:20:20 +0530

    Complete 'Sum Mixed Array' challenge

---
+++ 8kyu/Sum Mixed Array.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+//My Solution
+function sumMix(x){
+    return x.reduce((acc, cur) => acc + Number(cur), 0)
+  }
