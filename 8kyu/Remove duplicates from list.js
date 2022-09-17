9838ef833598f03be589315dca0d01c783a8ed35
devmev10 <ravitokas90@gmail.com>
2022-09-17 17:13:10 +0530

    Complete 'Remove duplicates from list' codewars challenge

---
+++ 8kyu/Remove duplicates from list.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+//My solution
+function distinct(a) {
+    let b = a.filter( (item, index, arr) =>  arr.indexOf(item) === index  )
+    return b
+  }
