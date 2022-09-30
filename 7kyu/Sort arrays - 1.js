0dd9fdcd804e9793c46cde2ce5495c74f79512fa
devmev10 <ravitokas90@gmail.com>
2022-09-30 14:18:11 +0530

    Sort arrays - 1

---
+++ 7kyu/Sort arrays - 1.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+// My Solution
+
+sortme = function( names ){
+    return names.sort()
+  }
