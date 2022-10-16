cb825f4d8ae0e8a33716f86daca034240c94078b
devmev10 <ravitokas90@gmail.com>
2022-10-16 15:07:07 +0530

    Complete 'Reverse words' challenge

---
+++ 7kyu/Reverse words.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+// My Solution
+function reverseWords(str) {
+  
+    return str.split(' ').map( item => item.split('').reverse().join('')).join(' ')
+   
+   }
+   reverseWords('This is an example!')
