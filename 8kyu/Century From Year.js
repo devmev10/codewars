de4a4397ccdc68499a983df335e0e3c780345e99
devmev10 <ravitokas90@gmail.com>
2022-10-22 10:34:32 +0530

    Complete 'Century From Year' challenge

---
+++ 8kyu/Century From Year.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// My Solution
+function century(year) {
+    return year%100 === 0 ? year/100 : Math.floor(year/100) + 1
+     
+  }
