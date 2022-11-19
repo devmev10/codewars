492bb6cee6febd8ba2fb6edc2ecb4725c339e6c6
devmev10 <ravitokas90@gmail.com>
2022-11-19 09:58:43 +0530

    Complete 'Sum of angles' challenge

---
+++ 7kyu/Sum of angles.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+// My Solution
+function angle(n) {
+    return 180 * (n-2)
+  }
