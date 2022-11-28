fad9386c4191aaf87d7378474b762fde0ff028a1
devmev10 <ravitokas90@gmail.com>
2022-11-28 12:07:11 +0530

    Complete ' Find the position' challenge

---
+++ 8kyu/Find the position.js
@@ -0,0 +1,12 @@
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+
+// My Solution
+function position(letter){
+    let arr = [0, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    return `Position of alphabet: ${arr.indexOf(letter)}`
+    }
