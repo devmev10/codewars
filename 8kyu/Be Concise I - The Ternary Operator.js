16ce93725c1a159acfd637193b2a9fcfec512332
Ravi Tokas <110528769+devmev10@users.noreply.github.com>
2022-12-21 10:04:17 +0530

    Update Be Concise I - The Ternary Operator.js

--- 8kyu/Be Concise I - The Ternary Operator.js
+++ 8kyu/Be Concise I - The Ternary Operator.js
@@ -9,15 +9,5 @@
 
 // My Solution
 function describeAge(age) {
-//   if (age <= 12) {
-//     return "You're a(n) kid";
-//   } else if (age >= 13 && age <= 17) {
-//     return "You're a(n) teenager";
-//   } else if (age >= 18 && age <= 64) {
-//     return "You're a(n) adult";
-//   } else {
-//     return "You're a(n) elderly";
-//   }
-  
   return `You're a(n) ${age > 64? 'elderly' : age>= 18? 'adult' : age>=13? 'teenager' : 'kid'}`
 }
