cae43819222d6d2d5b498067ed7cc3f11964186c
Ravi Tokas <110528769+devmev10@users.noreply.github.com>
2022-12-24 09:52:34 +0530

    Complete 'Grader' challenge

---
+++ 8kyu/Grader.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// Create a function that takes a number as an argument and returns a grade based on that number.
+
+// Score	Grade
+// Anything greater than 1 or less than 0.6	"F"
+// 0.9 or greater	"A"
+// 0.8 or greater	"B"
+// 0.7 or greater	"C"
+// 0.6 or greater	"D"
+
+// My Solution
+function grader(score) {
+  return score> 1 || score < 0.6? 'F':score>= 0.9 ? 'A' : score>= 0.8? 'B': score>= 0.7 ? 'C' : 'D'
+}
