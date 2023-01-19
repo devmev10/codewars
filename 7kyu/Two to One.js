31bc3eecfdace8f13a3d347faa5f04b783e9c129
Ravi Tokas <110528769+devmev10@users.noreply.github.com>
2023-01-19 03:45:17 +0000

    Complete 'Two to One' challenge

---
+++ 7kyu/Two to One.js
@@ -0,0 +1,18 @@
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+// My Solution
+function longest(s1, s2) {
+  let arr = (s1 + s2).split("");
+  return arr
+    .filter((item, index) => arr.indexOf(item) === index)
+    .sort()
+    .join("");
+}
