9aecf2c5eed00d9380ed5d4c77babf7acde85e44
Ravi Tokas <110528769+devmev10@users.noreply.github.com>
2023-07-15 04:34:16 +0000

    Help Suzuki count his vegetables

---
+++ 7kyu/Help Suzuki count his vegetables.js
@@ -0,0 +1,39 @@
+// Help Suzuki count his vegetables....
+
+// Suzuki is the master monk of his monastery so it is up to him to ensure the kitchen is operating at full capacity to feed his students and the villagers that come for lunch on a daily basis.
+
+// This week there was a problem with his deliveries and all the vegetables became mixed up. There are two important aspects of cooking in his kitchen, it must be done in harmony and nothing can be wasted. Since the monks are a record keeping people the first order of business is to sort the mixed up vegetables and then count them to ensure there is enough to feed all the students and villagers.
+
+// You will be given a string with the following vegetables:
+
+// "cabbage", "carrot", "celery", "cucumber", "mushroom", "onion", "pepper", "potato", "tofu", "turnip"
+// Return a list of tuples with the count of each vegetable in descending order. If there are any non vegetables mixed in discard them. If the count of two vegetables is the same sort in reverse alphabetical order (Z->A).
+
+// (119, "pepper"),
+// (114, "carrot"),
+// (113, "turnip"),
+// (102, "onion"),
+// (101, "tofu"),
+// (100, "cabbage"),
+// (93, "mushroom"),
+// (90, "cucumber"),
+// (88, "potato"),
+// (80, "celery")
+// My Solution
+
+const countVegetables = str => {
+    let tuples = [];
+    let counts = str
+      .match(/tofu|potato|cucumber|cabbage|turnip|pepper|onion|mushroom|celery|carrot/g)
+      .reduce((res, word) => {
+        res[word] = (res[word] || 0) + 1;
+        return res;
+      }, {});
+    
+    for (var word in counts)
+      tuples.push([counts[word], word]);
+    
+    tuples = tuples.sort((a, b) => b[0] - a[0] || b[1].localeCompare(a[1]));
+    
+    return tuples;
+  }
