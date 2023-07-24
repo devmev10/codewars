b507d02b554a5839bf96a7d3efc84fb00b196af2
Ravi Tokas <110528769+devmev10@users.noreply.github.com>
2023-07-24 15:28:00 +0530

    Help Suzuki rake his garden!

---
+++ 7kyu/Help Suzuki rake his garden!.js
@@ -0,0 +1,29 @@
+// Help Suzuki rake his garden!
+
+// The monastery has a magnificent Zen garden made of white gravel and rocks and it is raked diligently everyday by the monks. Suzuki having a keen eye is always on the lookout for anything creeping into the garden that must be removed during the daily raking such as insects or moss.
+
+// You will be given a string representing the garden such as:
+
+// garden = 'gravel gravel gravel gravel gravel gravel gravel gravel gravel rock slug ant gravel gravel snail rock gravel gravel gravel gravel gravel gravel gravel slug gravel ant gravel gravel gravel gravel rock slug gravel gravel gravel gravel gravel snail gravel gravel rock gravel snail slug gravel gravel spider gravel gravel gravel gravel gravel gravel gravel gravel moss gravel gravel gravel snail gravel gravel gravel ant gravel gravel moss gravel gravel gravel gravel snail gravel gravel gravel gravel slug gravel rock gravel gravel rock gravel gravel gravel gravel snail gravel gravel rock gravel gravel gravel gravel gravel spider gravel rock gravel gravel'
+// Rake out any items that are not a rock or gravel and replace them with gravel such that:
+
+// garden = 'slug spider rock gravel gravel gravel gravel gravel gravel gravel'
+// Returns a string with all items except a rock or gravel replaced with gravel:
+
+// garden = 'gravel gravel rock gravel gravel gravel gravel gravel gravel gravel'
+
+// My Solution
+function rakeGarden(garden) {
+  // Split the garden string into an array of words
+  const gardenArray = garden.split(" ");
+
+  // Loop through the array and replace items that are not "rock" with "gravel"
+  for (let i = 0; i < gardenArray.length; i++) {
+    if (gardenArray[i] !== "rock") {
+      gardenArray[i] = "gravel";
+    }
+  }
+
+  // Join the array back into a string and return the result
+  return gardenArray.join(" ");
+}
