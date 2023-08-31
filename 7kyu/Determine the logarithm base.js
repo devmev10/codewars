78090993061f1041b36d2eec6553d6de49b8c47c
Ravi Tokas <110528769+devmev10@users.noreply.github.com>
2023-08-31 03:47:32 +0000

    Determine the logarithm base

---
+++ 7kyu/Determine the logarithm base.js
@@ -0,0 +1,7 @@
+// Define a function that will receive a logarithm function, and returns the base of that logarithm.
+
+// guessBase(ln) == e
+// Base is a real number (not only integers) guaranteed to be less than 1e6.
+
+// My Solution
+determineBase=f=>2**f(2)**-1
