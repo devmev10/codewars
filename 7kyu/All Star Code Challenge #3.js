618e80229976f51e2c58952abcfef754f23286d8
Ravi Tokas <110528769+devmev10@users.noreply.github.com>
2023-09-17 05:42:00 +0000

    All Star Code Challenge #3

---
+++ 7kyu/All Star Code Challenge #3.js
@@ -0,0 +1,14 @@
+// Create a function that takes a string argument and returns that same string with all vowels removed (vowels are "a", "e", "i", "o", "u").
+
+// Example (Input --> Output)
+
+// "drake" --> "drk"
+// "aeiou" --> ""
+// remove_vowels("drake") // => "drk"
+// remove_vowels("aeiou") // => ""
+
+// My Solution
+
+function removeVowels(str) {
+    return str.replace(/[aeiou]/gi, ''); //hehe
+  }
