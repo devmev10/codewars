e1d7f64506db29787956a82afba7151ba6f6629e
Ravi Tokas <110528769+devmev10@users.noreply.github.com>
2023-09-23 19:11:20 +0530

    Number climber

---
+++ 7kyu/Number climber.js
@@ -0,0 +1,13 @@
+// For every positive integer N, there exists a unique sequence starting with 1 and ending with N and such that every number in the sequence is either the double of the preceeding number or the double plus 1.
+
+// For example, given N = 13, the sequence is [1, 3, 6, 13], because . . . :
+
+//  3 =  2*1 +1
+//  6 =  2*3
+//  13 = 2*6 +1
+// Write a function that returns this sequence given a number N. Try generating the elements of the resulting list in ascending order, i.e., without resorting to a list reversal or prependig the elements to a list.
+
+// My Solution
+
+const climb = (n) =>
+  [...n.toString(2)].map((_, idx, arr) => n >> (arr.length - ++idx));
