d0bd3354bf22a68e6e0dec60b9828c000615c947
Ravi Tokas <110528769+devmev10@users.noreply.github.com>
2023-09-24 12:27:30 +0530

    Infinitely Nested Radicals

---
+++ 7kyu/Infinitely Nested Radicals.js
@@ -0,0 +1,35 @@
+// (there are an infinite number of radicals)
+
+// �
+// +
+// �
+// +
+// �
+// +
+// �
+// +
+// ⋯
+// x+
+// x+
+// x+
+// x+⋯
+// ​
+
+// ​
+
+// ​
+
+// ​
+
+// for a given value x
+
+// Simple!
+
+// arguments passed in will be 1 or greater
+
+// Image not loaded so please refer the codewar at https://www.codewars.com/kata/5af2b240d2ee2764420000a2/train/javascript
+
+// My Solution
+function fn(x) {
+  return (1 + Math.sqrt(1 + 4 * x)) / 2;
+}
