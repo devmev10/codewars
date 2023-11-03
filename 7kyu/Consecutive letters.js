d8f4f7bff30c849c7b84b5ca07b10b8a1463d204
Ravi Tokas <110528769+devmev10@users.noreply.github.com>
2023-11-03 08:37:39 +0530

    Consecutive letters

---
+++ 7kyu/Consecutive letters.js
@@ -0,0 +1,28 @@
+// In this Kata, we will check if a string contains consecutive letters as they appear in the English alphabet and if each letter occurs only once.
+
+// Rules are: (1) the letters are adjacent in the English alphabet, and (2) each letter occurs only once.
+
+// For example:
+// solve("abc") = True, because it contains a,b,c
+// solve("abd") = False, because a, b, d are not consecutive/adjacent in the alphabet, and c is missing.
+// solve("dabc") = True, because it contains a, b, c, d
+// solve("abbc") = False, because b does not occur once.
+// solve("v") = True
+// All inputs will be lowercase letters.
+
+// My Solution
+function solve(s) {
+  if (s.length === 1) {
+    return true;
+  }
+
+  const sortedChars = s.split("").sort().join("");
+
+  for (let i = 1; i < sortedChars.length; i++) {
+    if (sortedChars.charCodeAt(i) - sortedChars.charCodeAt(i - 1) !== 1) {
+      return false;
+    }
+  }
+
+  return true;
+}
